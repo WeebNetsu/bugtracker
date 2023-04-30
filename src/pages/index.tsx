d5220b7160a3c042baa32ca5cb567f40748b8362
WeebNetsu <stephenvdw7777@gmail.com>
2023-04-30 20:46:37 +0200

    Added project page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { Space, Typography } from "antd";
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <>
-            <Link href={"/login"}>Login</Link>
-        </>
+        <Space direction="vertical">
+            <Typography>Navigation</Typography>
+            <Space>
+                <Link href={"/login"}>Login</Link>
+                <Link href={"/signup"}>Signup</Link>
+            </Space>
+
+            <Typography>Your Projects:</Typography>
+            <Typography>
+                <Link href={"/project/12edwqasdasda/"}>Dummy Project</Link>
+            </Typography>
+        </Space>
     );
 }
---
+++ src/pages/project/[id]/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "antd";
+import React from "react";
+
+const SpecificProjectPage: React.FC = () => {
+    return (
+        <>
+            <Typography.Title>Hello World</Typography.Title>
+        </>
+    );
+};
+
+export default SpecificProjectPage;
