295821c3ec1c2c4ff89eb591178ebce8b65032bb
WeebNetsu <stephenvdw7777@gmail.com>
2023-05-05 21:16:43 +0200

    Implemented adding more spaces

--- src/pages/project/[id]/index.tsx
+++ src/pages/project/[id]/index.tsx
@@ -1,18 +1,21 @@
 import Loader from "@/components/loader";
 import ProjectModel from "@/models/project";
-import { SingleProjectGetResponseModel } from "@/pages/api/projects/[id]/_models";
-import { parseApiResponse, sendGetRequest, uiHandleRequestFailed } from "@/utils/requests";
-import { formatToHumanDate } from "@netsu/js-utils";
+import { AvailableRequestMethods } from "@/models/requests";
+import { SingleProjectGetResponseModel, SingleProjectPutRequestBodyModel } from "@/pages/api/projects/[id]/_models";
+import { parseApiResponse, sendGetRequest, sendPostRequest, uiHandleRequestFailed } from "@/utils/requests";
+import { checkStrEmpty, formatToHumanDate } from "@netsu/js-utils";
 import { useUser } from "@supabase/auth-helpers-react";
-import { Space, Typography, message } from "antd";
+import { Button, Input, Space, Typography, message } from "antd";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import StatusItem from "./_statusItem";
 
 const SpecificProjectPage: React.FC = () => {
     const user = useUser();
     const [loading, setLoading] = useState(true);
     const [userProject, setUserProject] = useState<ProjectModel | undefined>();
+    const [newStatusTitle, setNewStatusTitle] = useState("");
     const router = useRouter();
 
     useEffect(() => {
@@ -53,6 +56,44 @@ const SpecificProjectPage: React.FC = () => {
         getData();
     }, [user, router.query]);
 
+    const handleAddNewStatus = async () => {
+        if (checkStrEmpty(newStatusTitle)) {
+            return message.error("Title cannot be empty");
+        }
+
+        const { id } = router.query;
+
+        if (!id || typeof id !== "string" || !userProject) {
+            return message.error("Could not get project ID");
+        }
+
+        const { statuses = [] } = userProject;
+
+        statuses.push({
+            _id: uuidv4(),
+            orderIndex: statuses.length,
+            title: newStatusTitle,
+        });
+
+        const updatedProject = { ...userProject, statuses };
+
+        setUserProject(updatedProject);
+
+        const updateStatusData: SingleProjectPutRequestBodyModel = {
+            data: {
+                statuses: updatedProject.statuses,
+            },
+        };
+
+        const updated = await sendPostRequest(`/api/projects/${id}`, updateStatusData, AvailableRequestMethods.PUT);
+
+        if (!updated.ok) {
+            return message.error("Could not update project statuses");
+        }
+
+        setNewStatusTitle("");
+    };
+
     if (loading) return <Loader />;
 
     if (!userProject) return <Typography>Project not found</Typography>;
@@ -69,10 +110,12 @@ const SpecificProjectPage: React.FC = () => {
             <Typography>Created on {formatToHumanDate(userProject.createdAt)}</Typography>
 
             <Space
+                className="hideScrollbar"
                 style={{
                     width: "100%",
+                    alignItems: "flex-start",
+                    overflowX: "scroll",
                 }}
-                // size={"middle"}
             >
                 {userProject.statuses
                     .sort((prev, curr) => prev.orderIndex - curr.orderIndex)
@@ -84,6 +127,21 @@ const SpecificProjectPage: React.FC = () => {
                             userProject={userProject}
                         />
                     ))}
+
+                <div
+                    style={{
+                        width: "20rem",
+                    }}
+                >
+                    <Space>
+                        <Input
+                            placeholder="New Status"
+                            value={newStatusTitle}
+                            onChange={e => setNewStatusTitle(e.target.value)}
+                        />
+                        <Button onClick={handleAddNewStatus}>Add Status</Button>
+                    </Space>
+                </div>
             </Space>
         </Space>
     );
